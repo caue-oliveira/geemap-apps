2a79edddec8e4322c8b86e5b0365a8fa28ef0033
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-12 11:42:37 -0300

    Update nlcd_demo.py

--- pages/nlcd_demo.py
+++ pages/nlcd_demo.py
@@ -1,62 +1,48 @@
-# Importando as parada
 import ee
 import streamlit as st
 import geemap.foliumap as geemap
 
-# Configs página
-    #Título
-st.title("NDVI")
+# Get an NLCD image by year.
+def getNLCD(year):
+    # Import the NLCD collection.
+    dataset = ee.ImageCollection("USGS/NLCD_RELEASES/2019_REL/NLCD")
 
-    # Colunas
-col1, col2 = st.columns([4, 1])
-options = list(geemap.basemaps.keys())
-index = options.index("HYBRID")
+    # Filter the collection by year.
+    nlcd = dataset.filter(ee.Filter.eq("system:index", year)).first()
 
-with col2:
-
-    basemap = st.selectbox("Select a basemap:", options, index)
-
-with col1:
-# Mapa default
-    Map = geemap.Map(center=[-15.7801, -47.9292], zoom=12)
-    Map.add_basemap(basemap)
-    Map.to_streamlit(height=700)
-
-# Área referência
-coordinates_df = [
-    [-48.2973, -15.4973],  # Sudoeste
-    [-48.2973, -15.9761],  # Noroeste
-    [-47.3894, -15.9761],  # Nordeste
-    [-47.3894, -15.4973],  # Sudeste
-    [-48.2973, -15.4973]  # Fechar polígono
-]
-
-bsb = ee.Geometry.Polygon(coordinates_df)
-
-# Carregar a coleção de imagens Landsat-8 para o período especificado
-image = ee.ImageCollection("LANDSAT/LC08/C02/T1_L2") \
-    .filterBounds(bsb) \
-    .filter(ee.Filter.lt('CLOUD_COVER', 1))
+    # Select the land cover band.
+    landcover = nlcd.select("landcover")
+    return landcover
 
 
-# Função NDVI
-def ndvi(img):
-    # Selecionar as bandas ópticas e aplicar o fator de escala
-    opticalBands = img.select(['SR_B1', 'SR_B2', 'SR_B3', 'SR_B4', 'SR_B5', 'SR_B6', 'SR_B7'])\
-                       .multiply(0.0000275).add(-0.2)
+st.header("National Land Cover Database (NLCD)")
 
-    # Calcular o NDVI
-    ndvi_image = img.normalizedDifference(['SR_B5', 'SR_B4']).rename('NDVI')
+# Create a layout containing two columns, one for the map and one for the layer dropdown list.
+row1_col1, row1_col2 = st.columns([3, 1])
 
-    # Adicionar bandas ópticas e NDVI à imagem
-    return img.addBands(opticalBands).addBands(ndvi_image)\
-              .copyProperties(img, ['system:time_start'])\
-              .set('date', img.date().format('YYYY-MM-dd'))
+# Create an interactive map
+Map = geemap.Map(center=[40, -100], zoom=4)
 
-# Aplicar a função à coleção de imagens
-collection_ndvi = image.map(ndvi)
+# Select the seven NLCD epochs after 2000.
+years = ["2001", "2004", "2006", "2008", "2011", "2013", "2016", "2019"]
 
-# Adicionar a camada NDVI ao mapa
-Map.addLayer(collection_ndvi,
-             {'bands': 'NDVI', 'min': -0.3, 'max': 0.65, 'palette': ['B62F02', 'D87B32','FCF40D','62C41C','0A5C1C']},
-             'NDVI')
+# Add a dropdown list and checkbox to the second column.
+with row1_col2:
+    selected_year = st.multiselect("Select a year", years)
+    add_legend = st.checkbox("Show legend")
+
+# Add selected NLCD image to the map based on the selected year.
+if selected_year:
+    for year in selected_year:
+        Map.addLayer(getNLCD(year), {}, "NLCD " + year)
+
+    if add_legend:
+        Map.add_legend(
+            title="NLCD Land Cover Classification", builtin_legend="NLCD"
+        )
+    with row1_col1:
+        Map.to_streamlit(height=600)
+
+else:
+    with row1_col1:
+        Map.to_streamlit(height=600)
