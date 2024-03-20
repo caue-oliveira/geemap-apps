6f6e7eb830976d6d45d3ed20f9434587567f1d1f
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-20 15:37:13 -0300

    Update NDVI_v2.py

--- pages/NDVI_v2.py
+++ pages/NDVI_v2.py
@@ -60,7 +60,7 @@ def app():
 
     with row1_col1:
         ee_authenticate(token_name="EARTHENGINE_TOKEN")
-        m = folium.Map(
+        m = geemap.Map(
             basemap="HYBRID",
             plugin_Draw=True,
             Draw_export=True,
@@ -249,11 +249,7 @@ def app():
                                 "An error occurred: " + str(e)
                             )
                             st.stop()
-
-                        m.add_layer(ndvi,
-                                    {'min': -0.2, 'max': 1,
-                                     'palette': ['B62F02', 'D87B32', 'FCF40D', '62C41C', '0A5C1C']}, 'NDVI'
-                                    )
+                            
                 else:
                     empty_text.error(
                         "Something went wrong. You probably requested too much data. Try reducing the ROI or timespan."
