867446c1092a4727cf6f86342fc2f7f4143f9898
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-20 15:42:29 -0300

    Update NDVI_v2.py

--- pages/NDVI_v2.py
+++ pages/NDVI_v2.py
@@ -227,7 +227,6 @@ def app():
                                 img_collection = (
                                     ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
                                     .filterBounds(roi)
-                                    .filterDate(start_date, end_date)
                                     .sort('CLOUD_COVER')
                                 )
 
@@ -236,7 +235,7 @@ def app():
                                 clip_sr_img = img_filter.clip(roi).multiply(0.0000275).add(-0.2)
                                 ndvi = clip_sr_img.normalizedDifference(['SR_B5', 'SR_B4'])
                                 m.add_layer(ndvi)
-                                            
+
                                 count = img_collection.size().getInfo()
                                 empty_text.error("Quantidade de imagens disponíveis: " + str(count))
 
