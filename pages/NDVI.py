5cf6b8a0a8acd7cd555b1602cb649b298a68d886
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-20 17:14:12 -0300

    Update NDVI.py

--- pages/NDVI.py
+++ pages/NDVI.py
@@ -266,7 +266,7 @@ def app():
                                             )
                                 count = img_collection.size().getInfo()
                                 empty_text.error("Total image available: " + str(count))
-                                download_url.warning("Tot321321321321321e: " + str(count))
+                                download_url.warning(ndvi.getDownloadURL)
 
                             elif collection == "Sentinel-2 MSI Surface Reflectance":
                                 empty_text.error("Sentinel in progress")
