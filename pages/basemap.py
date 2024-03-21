ebd74e533deb45e0bd834e44a91dce495bafa112
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-21 16:35:10 -0300

    Update basemap.py

--- pages/basemap.py
+++ pages/basemap.py
@@ -1,81 +1,64 @@
 import streamlit as st
-import geemap.foliumap as geemap
-import ee
-import folium
+import leafmap.foliumap as leafmap
 
-st.title("Interactive Map")
+st.set_page_config(layout="wide")
 
-col1, col2 = st.columns([4, 1])
-options = list(geemap.basemaps.keys())
-index = options.index("OpenTopoMap")
+st.sidebar.info(
+    """
+    - Web App URL: <https://streamlit.geemap.org>
+    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
+    """
+)
 
-# Earth Engine drawing method setup
-def add_ee_layer(self, ee_image_object, vis_params, name):
-    map_id_dict = ee.Image(ee_image_object).getMapId(vis_params)
-    layer = folium.raster_layers.TileLayer(
-        tiles=map_id_dict['tile_fetcher'].url_format,
-        attr='Map Data &copy; <a href="https://earthengine.google.com/">Google Earth Engine</a>',
-        name=name,
-        overlay=True,
-        control=True
-    )
-    layer.add_to(self)
-    return layer
-
-# Configuring Earth Engine display rendering method in Folium
-folium.Map.add_ee_layer = add_ee_layer
-
-with col2:
-
-    basemap = st.selectbox("Select a basemap:", options, index)
+st.sidebar.title("Contact")
+st.sidebar.info(
+    """
+    Qiusheng Wu: <https://wetlands.io>
+    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
+    """
+)
 
 
-with col1:
-    # Defina o mapa globalmente
-    m = geemap.Map(
-        basemap="HYBRID",
-        plugin_Draw=True,
-        Draw_export=True,
-        locate_control=True,
-        plugin_LatLngPopup=False,
+def app():
+    st.title("Search Basemaps")
+    st.markdown(
+        """
+    This app is a demonstration of searching and loading basemaps from [xyzservices](https://github.com/geopandas/xyzservices) and [Quick Map Services (QMS)](https://github.com/nextgis/quickmapservices). Selecting from 1000+ basemaps with a few clicks.  
+    """
     )
 
-    m.add_basemap("HYBRID")
-    m.add_basemap(basemap)
+    with st.expander("See demo"):
+        st.image("https://i.imgur.com/0SkUhZh.gif")
 
-bsb = [
-    [-48.2973, -15.4973],  # Sudoeste
-    [-48.2973, -15.9761],  # Noroeste
-    [-47.3894, -15.9761],  # Nordeste
-    [-47.3894, -15.4973],  # Sudeste
-    [-48.2973, -15.4973]  # Fechar polígono
-]
-sample_roi = ee.Geometry.Polygon(bsb)
+    row1_col1, row1_col2 = st.columns([3, 1])
+    width = 800
+    height = 600
+    tiles = None
 
-collection = "Landsat TM-ETM-OLI Surface Reflectance"
+    with row1_col2:
 
-if collection == "Landsat TM-ETM-OLI Surface Reflectance":
-    img_collection = (
-        ee.ImageCollection("LANDSAT/LC08/C02/T1_L2")
-        .filterBounds(sample_roi)
-        .sort('CLOUD_COVER')
-    )
+        checkbox = st.checkbox("Search Quick Map Services (QMS)")
+        keyword = st.text_input("Enter a keyword to search and press Enter:")
+        empty = st.empty()
 
-    # Seleciona a primeira imagem da coleção
-    img = img_collection.first()
+        if keyword:
+            options = leafmap.search_xyz_services(keyword=keyword)
+            if checkbox:
+                qms = leafmap.search_qms(keyword=keyword)
+                if qms is not None:
+                    options = options + qms
 
-    # Clip da imagem
-    clip_sr_img = img.clip(sample_roi).multiply(0.0000275).add(-0.2)
+            tiles = empty.multiselect(
+                "Select XYZ tiles to add to the map:", options)
 
-    # Calcula o NDVI
-    ndvi = clip_sr_img.normalizedDifference(['SR_B5', 'SR_B4'])
+        with row1_col1:
+            m = leafmap.Map()
 
-    # Adiciona o NDVI ao mapa
-    m.addLayer(ndvi, {'min': -0.2, 'max': 1, 'palette': ['B62F02', 'D87B32', 'FCF40D', '62C41C', '0A5C1C']}, 'NDVI')
+            if tiles is not None:
+                for tile in tiles:
+                    m.add_xyz_service(tile)
 
-count = img_collection.size().getInfo()
-print("Quantidade de imagens na coleção:", count)
+            m.to_streamlit(height=height)
 
 
-
-m.to_streamlit(height=700)
+app()
