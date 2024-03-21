3b6c64c869567090b17c3f1bbb2df29fe382baf4
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-21 16:36:34 -0300

    Update basemap.py

--- pages/basemap.py
+++ pages/basemap.py
@@ -1,64 +1,19 @@
 import streamlit as st
-import leafmap.foliumap as leafmap
+import geemap.foliumap as geemap
 
-st.set_page_config(layout="wide")
+st.title("Interactive Map")
 
-st.sidebar.info(
-    """
-    - Web App URL: <https://streamlit.geemap.org>
-    - GitHub repository: <https://github.com/giswqs/streamlit-geospatial>
-    """
-)
+col1, col2 = st.columns([4, 1])
+options = list(geemap.basemaps.keys())
+index = options.index("OpenTopoMap")
 
-st.sidebar.title("Contact")
-st.sidebar.info(
-    """
-    Qiusheng Wu: <https://wetlands.io>
-    [GitHub](https://github.com/giswqs) | [Twitter](https://twitter.com/giswqs) | [YouTube](https://www.youtube.com/c/QiushengWu) | [LinkedIn](https://www.linkedin.com/in/qiushengwu)
-    """
-)
+with col2:
+
+    basemap = st.selectbox("Select a basemap:", options, index)
 
 
-def app():
-    st.title("Search Basemaps")
-    st.markdown(
-        """
-    This app is a demonstration of searching and loading basemaps from [xyzservices](https://github.com/geopandas/xyzservices) and [Quick Map Services (QMS)](https://github.com/nextgis/quickmapservices). Selecting from 1000+ basemaps with a few clicks.  
-    """
-    )
+with col1:
 
-    with st.expander("See demo"):
-        st.image("https://i.imgur.com/0SkUhZh.gif")
-
-    row1_col1, row1_col2 = st.columns([3, 1])
-    width = 800
-    height = 600
-    tiles = None
-
-    with row1_col2:
-
-        checkbox = st.checkbox("Search Quick Map Services (QMS)")
-        keyword = st.text_input("Enter a keyword to search and press Enter:")
-        empty = st.empty()
-
-        if keyword:
-            options = leafmap.search_xyz_services(keyword=keyword)
-            if checkbox:
-                qms = leafmap.search_qms(keyword=keyword)
-                if qms is not None:
-                    options = options + qms
-
-            tiles = empty.multiselect(
-                "Select XYZ tiles to add to the map:", options)
-
-        with row1_col1:
-            m = leafmap.Map()
-
-            if tiles is not None:
-                for tile in tiles:
-                    m.add_xyz_service(tile)
-
-            m.to_streamlit(height=height)
-
-
-app()
+    m = geemap.Map()
+    m.add_basemap(basemap)
+    m.to_streamlit(height=700)
