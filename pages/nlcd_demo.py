f7065bdd3fa406b6b04a9d2c0e51ac440bc9b397
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-22 01:16:40 -0300

    Update nlcd_demo.py

--- pages/nlcd_demo.py
+++ pages/nlcd_demo.py
@@ -32,11 +32,46 @@ folium.Marker(
 ).add_to(m)
 
 colors = {
-    'D1f': '#ff0000',  # Cor para 'Valor1'
-    'JKλian': '#00ff00',  # Cor para 'Valor2'
-    'JKλiaga': '#0000ff',
-    'JKλiasv': 'D87B32',
-    'NP1apoa':'#0A5C1C',
+    {
+        'D1f': '#ff0000',  # Cor para 'D1f'
+        'JKλian': '#00ff00',  # Cor para 'JKλian'
+        'JKλiaga': '#0000ff',  # Cor para 'JKλiaga'
+        'JKλiasv': '#ff00ff',  # Cor para 'JKλiasv'
+        'NP1apoa': '#ffff00',  # Cor para 'NP1apoa'
+        'NP1apoum': '#00ffff',  # Cor para 'NP1apoum'
+        'NPαch': '#ff9900',  # Cor para 'NPαch'
+        'NP1apach': '#9900ff',  # Cor para 'NP1apach'
+        'NP1apam': '#0099ff',  # Cor para 'NP1apam'
+        'NP1apaq': '#ff0099',  # Cor para 'NP1apaq'
+        'NP1apax': '#990000',  # Cor para 'NP1apax'
+        'NP1γnacc': '#009900',  # Cor para 'NP1γnacc'
+        'PP3γr': '#990099',  # Cor para 'PP3γr'
+        'NP3γsnird': '#999999',  # Cor para 'NP3γsnird'
+        'NP3γsnirm': '#660000',  # Cor para 'NP3γsnirm'
+        'NP3γsnirsv': '#006600',  # Cor para 'NP3γsnirsv'
+        'NP2γst': '#000066',  # Cor para 'NP2γst'
+        'NP2aγalgf': '#660066',  # Cor para 'NP2aγalgf'
+        'NP2aγalg': '#666600',  # Cor para 'NP2aγalg'
+        'NP2aγalhbt': '#6600ff',  # Cor para 'NP2aγalhbt'
+        'NP3γsnirsf': '#ff6600',  # Cor para 'NP3γsnirsf'
+        'NP1apocc': '#00ff66',  # Cor para 'NP1apocc'
+        'JKλiaop': '#6600cc',  # Cor para 'JKλiaop'
+        'NP1γnaum': '#cc6600',  # Cor para 'NP1γnaum'
+        'NP1apaum': '#0066cc',  # Cor para 'NP1apaum'
+        'NP1apaa': '#cc0066',  # Cor para 'NP1apaa'
+        'NP3γcaigg': '#ccff00',  # Cor para 'NP3γcaigg'
+        'NP1γnaa': '#00ccff',  # Cor para 'NP1γnaa'
+        'NP3γsnirsg': '#ffcc00',  # Cor para 'NP3γsnirsg'
+        'NP3γcaigf': '#00ffcc',  # Cor para 'NP3γcaigf'
+        'NP1γnagt': '#cc00ff',  # Cor para 'NP1γnagt'
+        'NP3γcaid': '#ffcc99',  # Cor para 'NP3γcaid'
+        'NP1apox': '#99ffcc',  # Cor para 'NP1apox'
+        'NP3γsnirt': '#cc99ff',  # Cor para 'NP3γsnirt'
+        'NP3γsnirgr': '#ff99cc',  # Cor para 'NP3γsnirgr'
+        'Dpg': '#cc9999',  # Cor para 'Dpg'
+        'NP1apoch': '#99cc99',  # Cor para 'NP1apoch'
+        'NP1δmb': '#9999cc'  # Cor para 'NP1δmb'
+    }
     # Adicione mais cores conforme necessário para outros valores de 'Sigla'
 }
 
