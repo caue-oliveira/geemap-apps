163da869d52705e143b01a0e37dbde645d3a36c3
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-22 00:35:04 -0300

    Update nlcd_demo.py

--- pages/nlcd_demo.py
+++ pages/nlcd_demo.py
@@ -24,21 +24,11 @@ st.sidebar.info(
 
 st.title("Mapa Geológico do Projeto Arenópolis - TF 2023 UnB")
 
-'''# Inicializa um mapa Folium
+# Inicializa um mapa Folium
 m = folium.Map(location=[-16.39374927779391, -51.663956293293964], zoom_start=16)
 folium.LayerControl().add_to(m)
 # Adiciona o GeoDataFrame como GeoJson ao mapa
 #folium.GeoJson(unds).add_to(m)
 
 # Exibe o mapa no Streamlit
-st_data = st_folium(m, height=725, width=1500)'''
-# center on Liberty Bell, add marker
-m = folium.Map(location=[39.949610, -75.150282], zoom_start=16)
-folium.LayerControl().add_to(m)
-folium.Marker(
-    [39.949610, -75.150282], popup="Liberty Bell", tooltip="Liberty Bell"
-).add_to(m)
-
-# call to render Folium map in Streamlit, but don't get any data back
-# from the map (so that it won't rerun the app when the user interacts)
-st_folium(m, width=725, returned_objects=[])
+st_folium(m, height=725, width=1500)
