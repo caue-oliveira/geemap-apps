b89257f700f27323defe9fe5a5c7ad24691a236b
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-22 11:55:21 -0300

    Update basemap.py

--- pages/basemap.py
+++ pages/basemap.py
@@ -1,20 +1,6 @@
 import streamlit as st
 import geemap.foliumap as geemap
-import geopandas as gpd
 
-st.title("Interactive Map")
+st.title("Exposição de Mapas")
 
-col1, col2 = st.columns([4, 1])
-options = list(geemap.basemaps.keys())
-index = options.index("Hybrid")
-
-with col2:
-
-    basemap = st.selectbox("Select a basemap:", options, index)
-
-
-with col1:
-
-    m = geemap.Map()
-    m.add_basemap(basemap)
-    m.to_streamlit(height=700)
+st.subhead('Mapa de Localização de Projeto de Mineração')
