d7259db80aa3016ca0db712739f0838ae1c435b8
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-22 12:34:11 -0300

    map_class

--- pages/4_Mapas.py
+++ pages/4_Mapas.py
@@ -7,6 +7,8 @@ st.markdown('''
     Este mapa é produto de uma classificação supervisionada que realizei com o plugin Semi-Automatic Classification Plugin no QGIS na Região Administrativa do Plano Piloto em Brasília-DF.
            '''
             )
+st.image('data/mapa_lt.png')
+
 st.subheader('Mapa de Localização de Projeto de Mineração')
 st.markdown('''
     Escreva aqui
