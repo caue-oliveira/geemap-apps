0743e4d2b0c25cfcb079b459eda79a2b6b35d92e
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-22 15:10:37 -0300

    up5

--- pages/4_Mapas.py
+++ pages/4_Mapas.py
@@ -1,5 +1,19 @@
 import streamlit as st
 
+st.set_page_config(layout="wide")
+
+st.sidebar.title("Contato")
+st.sidebar.info(
+    """
+        Cauê Oliveira Miranda \n
+        caue.oliveira99@gmail.com \n
+        [GitHub](https://github.com/caue-oliveira) | [LinkedIn](https://www.linkedin.com/in/caueoliveira99) | [Currículo](https://www.canva.com/design/DAF4RWCBOuk/zsbheu6nUrXpw8PWQpcpjw/view?utm_content=DAF4RWCBOuk&utm_campaign=designshare&utm_medium=link&utm_source=viewer)
+    """
+)
+logo = "data/SGRS_LG.png"
+
+st.sidebar.image(logo)
+
 st.title("Exposição de Mapas")
 
 st.subheader('Mapa de Uso e Cobertura do Solo do Plano Piloto')
