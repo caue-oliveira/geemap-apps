731537f7044ca351125e68c26c4a9efe64a512e0
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-22 12:14:31 -0300

    up2

---
+++ pages/4_Mapas.py
@@ -0,0 +1,10 @@
+import streamlit as st
+import geemap.foliumap as geemap
+
+st.title("Exposição de Mapas")
+
+st.subheader('Mapa de Localização de Projeto de Mineração')
+st.markdown('''
+    Escreva aqui
+           '''
+            )
--- pages/basemap.py
+++
@@ -1,6 +0,0 @@
-import streamlit as st
-import geemap.foliumap as geemap
-
-st.title("Exposição de Mapas")
-
-st.subheader('Mapa de Localização de Projeto de Mineração')
