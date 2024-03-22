9d8b9ce779dcfffe3c44e81c3f30f4b3b60f4617
caue.oliveira <117476788+caue-oliveira@users.noreply.github.com>
2024-03-22 11:10:48 -0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,8 @@ st.set_page_config(layout="wide")
 
 # Customize the sidebar
 markdown = """
-    Cauê Oliveira Miranda
-    caue.oliveira99@gmail.com
+    Cauê Oliveira Miranda \n
+    caue.oliveira99@gmail.com \n
     [GitHub](https://github.com/caue-oliveira) | [LinkedIn](https://www.linkedin.com/in/qiushengwu) | [Currículo](https://www.canva.com/design/DAF4RWCBOuk/zsbheu6nUrXpw8PWQpcpjw/view?utm_content=DAF4RWCBOuk&utm_campaign=designshare&utm_medium=link&utm_source=viewer)
 """
     # Side bar info
